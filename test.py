f6d103398bec0b822136bf6608f031c28068ef6e
Yerim <yerimy97@gmail.com>
2020-06-08 17:58:50 +0900

    loop threw folder

--- test.py
+++ test.py
@@ -1,28 +1,38 @@
+import os
+
 import numpy as np
+
 import cv2
+
 from matplotlib import pyplot as plt
-img1 = cv2.imread('./DSC01739.JPG',0)
-img2 = cv2.imread('./DSC01741.JPG',0)
-img1 = cv2.resize(img1, dsize=(258, 258), interpolation=cv2.INTER_AREA)
-img2 = cv2.resize(img2, dsize=(258, 258), interpolation=cv2.INTER_AREA)
 
-sift = cv2.xfeatures2d.SIFT_create()
+import glob
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+from PIL import Image
 
-# BFMatcher with default params
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2, k=2)
+img_dir = "./img"
 
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
+data_path = img_dir + "/*.JPG"
 
-# cv2.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+files = glob.glob(data_path)
 
-plt.imshow(img3),plt.show()
+data = []
+
+print(files)
+
+for f1 in files:
+    img = cv2.imread(f1)
+
+    img = cv2.resize(img, dsize=(258, 258), interpolation=cv2.INTER_AREA)
+
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    kp = sift.detect(img, None)
+
+    img = cv2.drawKeypoints(img, kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    cv2.imshow('img', img)
+
+    cv2.waitKey()
+
+    cv2.destroyAllWindows()
